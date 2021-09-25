f2339f6013dfb0de8583e818974401f8c347f4be
İrem Atılgan <irematilgan99@gmail.com>
2021-09-25 14:25:31 +0300

    Outfits page for updating has been added

--- routes/outfits.js
+++ routes/outfits.js
@@ -1,29 +1,50 @@
-const express = require('express')
-const router = express.Router()
-const Outfit = require('../models/outfit')
-const ImageObj = require('../models/image')
+const express = require('express');
+const router = express.Router();
+const Outfit = require('../models/outfit');
+const ImageObj = require('../models/image');
+const Member = require('../models/member');
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) return next();
+    res.redirect('./login');
+}
 
 // All outfits route
-router.get('/', async (req,res) => {
+router.get('/', isLoggedIn, async (req,res) => {
 
-    params = {}
-    let outfitArr = []
+    params = {};
+    let outfitArr = [];
+    const userId = req.session.context;
 
     try {
-        const outfits = await Outfit.find({})
-        for(i = 0; i < outfits.length; i++) {
-            const imageTop = await ImageObj.findById(outfits[i].imageTop)
-            const imageBottom = await ImageObj.findById(outfits[i].imageBottom)
-            outfitArr.push({
-                imageTopImg : imageTop.clothingImagePath,
-                imageBottomImg : imageBottom.clothingImagePath,
-                score : outfits[i].score,
-                id : outfits[i].id
-            })
-            
+        const member = await Member.findOne({_id : userId}).exec();
+        if(typeof member.givenScores !== 'undefined') {
+            arrLen = member.givenScores.length;
+        } else {
+            arrLen = 0;
         }
+        console.log(arrLen);
+        for(i = 0; i < arrLen; i++) {
+            let outfit = await Outfit.find({_id : member.givenScores[i].outfitId}).exec();
 
-        res.render('./outfits/index',{outfits : outfitArr, errorMessage : null})
+            outfitDict = {
+                compScore : member.givenScores[i].score,
+                id : member.givenScores[i].outfitId,
+            };
+            outfitDict.images = [];
+            for(var j = 0; j < outfit[0].images.length; j++) {
+                var img = await ImageObj.findById(outfit[0].images[j]);
+                outfitDict.images.push(img.clothingImagePath);
+            }
+            
+            outfitArr.push(outfitDict)
+        }
+        //onsole.log(outfitArr[0]);
+        res.render('./outfits/index',{outfits : outfitArr,errorMessage : null, isLoggedIn : true});
+        
+        
+        
+        
     } catch (error) {
         console.log(error)
         res.render('./outfits/index', {errorMessage : 'Kombinler cagrilamadi'})
@@ -38,6 +59,41 @@ router.get('/new', async(req,res) => {
 
 // Add outfit to the database
 router.post('/', async(req,res) => {
+    console.log(req.body);
+    const newScore = req.body.newScore;
+    const memberId = req.session.context;
+    const outfitId = req.body.outfitId;
+    // members -> givenScores -> score
+    // outfits -> givenMembers -> score
+    try {
+        const currentOutfit = await Outfit.findById(outfitId);
+        var i = 0;
+        while(i < currentOutfit.givenMembers.length && currentOutfit.givenMembers[i].memberId != memberId) {
+            i++;
+        }
+        if(i < currentOutfit.givenMembers.length) {
+            currentOutfit.givenMembers[i].score = newScore;
+            await Outfit.updateOne({_id : outfitId},{$set : currentOutfit});
+        }
+        
+        const currentMember = await Member.findById(memberId);
+        var i = 0;
+        while(i < currentMember.givenScores.length && currentMember.givenScores[i].outfitId != outfitId) {
+            i++;
+        }
+        if(i < currentMember.givenScores.length) {
+            currentMember.givenScores[i].score = newScore;
+            await Member.updateOne({_id : memberId},{$set : currentMember});
+        }
+        console.log(currentOutfit);
+        console.log(currentMember);
+        res.redirect('../outfits/');
+
+    } catch(err) {
+        console.log(err);
+    }
+
+
 
 });
 
--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use('/login', loginRouter)
 app.use('/', indexRouter)
 app.use('/images', imageRouter)
 app.use('/about', aboutRouter)
-//app.use('/outfits', outfitRouter)
+app.use('/outfits', outfitRouter)
 app.use('/register', registerRouter)
 
 app.get('/logout', (req, res) => {
